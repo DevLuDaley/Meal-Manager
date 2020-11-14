2a655025a461f3612f71e03d647aa2d1723d80e7
devludaley <daleyluis@gmail.com>
2020-11-14 16:58:41 -0500

    add superuser

--- meal_manager_project/settings.py
+++ meal_manager_project/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = MEAL_MANAGER_SECRET_KEY
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         "NAME": "Meals-Manager",
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_PASS'),
+        "USER": config('MEALS_DB_USER'),
+        "PASSWORD": config('DB_PASS'),
         "HOST": "localhost",
         "PORT": "5432",
     }
